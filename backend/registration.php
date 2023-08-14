076ed688ac84ab9e657c576994ce06fc8eec71f3
Mahnoor Zahid <112409749+MahnoorZahid10@users.noreply.github.com>
2023-08-14 17:56:34 +0500

    Add files via upload

---
+++ backend/registration.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>IEEE Research Conference</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+ 
+  <!-- Favicons -->
+  <link href="../img/favicon.png" rel="icon">
+  <link href="../img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../vendor/aos/aos.css" rel="stylesheet">
+  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="../vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../css/style.css" rel="stylesheet">
+  <script src="https://cdn.logwork.com/widget/countdown.js"></script>
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+</head>
+<body  style=" background-image: url('../img/conf-bck-img4.jpg');">   
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top " style="background-color: #093955;">
+      <div class="container d-flex align-items-center justify-content-lg-between">
+        <a href="../index.html" class="logo me-auto me-lg-0"><img src="../img/logo.png" alt="" class="img-fluid"></a>
+  
+        <nav id="navbar" class="navbar order-last order-lg-0">
+          <ul>
+            <li><a class="nav-link scrollto " href="../index.html">Home</a></li>
+            <li><a class="nav-link scrollto" href="../about-us.html">About us</a></li>
+            <li class="dropdown"><a href="#"><span>Conference Comittee</span> <i class="bi bi-chevron-down"></i></a>
+              <ul>
+                <li><a class="nav-link scrollto" href="../org-committee.html">Organizing Committee</a></li>
+                <li><a class="nav-link scrollto" href="../tech-committee.html">Technical Committee</a></li>
+                <li><a class="nav-link scrollto" href="../keynote-speaker.html">Keynote Speakers</a></li>
+  
+              </ul>
+            </li>
+            <li><a class="nav-link scrollto " href="../gallery.html">Gallery</a></li>
+            <li><a class="nav-link scrollto " href="../archives.html">Archives</a></li>
+            <li><a class="nav-link scrollto " href="../sponsors.html">Sponsors</a></li>
+            <li><a class="nav-link scrollto " href="../call-for-papers.html">Call for papers</a></li>
+            <li><a class="nav-link scrollto" href="../contact-us.html">Contact Us</a></li>
+          </ul>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+  
+        <a href="../registration.html" class="get-started-btn scrollto">Register</a>
+  
+      </div>
+    </header><!-- End Header -->
+    <div class="container">
+
+    <div class="section-title" style="padding-top: 60px;">
+          <h2 style="color:white;">Attendee Registration</h2>
+        </div>
+        
+    <form method="POST"  class="registration-form">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="contact_number">Contact Number:</label>
+        <input type="text" id="contact_number" name="contact_number" required>
+
+        <label for="department">Profession:</label>
+        <input type="text" id="profession" name="profession" required>
+
+        <input type="submit" value="Register">
+    </form>
+    </div>
+     <!-- ======= Footer ======= -->
+     <footer id="footer">
+            <div class="footer-top">
+              <div class="container">
+                <div class="row">
+        
+                  <div class="col-lg-3 col-md-6">
+                    <p><img src="../img/logo.png" alt="" class="img-fluid"></p>
+                    <div class="footer-info">
+        
+                      <p>
+                      <h2 style="color: #6999b5;font-size:12px ; font-weight: bold;">IEEE Meetings, Conferences & Events</h2>
+                      <strong>Phone:</strong> +1 732 562 3878<br>
+                      <strong>Email:</strong> ieee-mce@ieee.org<br>
+                      </p>
+                      <div class="social-links mt-3">
+                        <a href="https://twitter.com/IEEEorg" class="twitter"><i class="bx bxl-twitter"></i></a>
+                        <a href="https://www.facebook.com/IEEE.org/" class="facebook"><i class="bx bxl-facebook"></i></a>
+                        <a href="https://www.instagram.com/ieeeorg/" class="instagram"><i class="bx bxl-instagram"></i></a>
+                        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                        <a href="https://www.linkedin.com/company/ieee" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                      </div>
+                    </div>
+                  </div>
+        
+                  <div class="col-lg-2 col-md-6 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                      <li><i class="bx bx-chevron-right"></i> <a href="index.html">Home</a></li>
+                      <li><i class="bx bx-chevron-right"></i> <a href="about-us.html">About us</a></li>
+                      
+                    </ul>
+                  </div>
+                  <div class="col-lg-3 col-md-6 footer-links">
+                    <h4></h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="registration.html">Register</a></li>
+                      <li><i class="bx bx-chevron-right"></i> <a href="sponsors.html">Sponsors</a></li>
+                      
+                    </ul>
+                  </div>
+        
+                  <div class="col-lg-4 col-md-6 footer-newsletter">
+                    <h4>Our Newsletter</h4>
+                    <form action="" method="post">
+                      <input type="email" name="email"><input type="submit" value="Subscribe">
+                    </form>
+        
+                  </div>
+        
+                </div>
+              </div>
+            </div>
+        
+            <div class="container">
+              <div class="copyright">
+                &copy; Copyright <strong><span>IEEE</span></strong>. All Rights Reserved
+              </div>
+            </div>
+          </footer><!-- End Footer -->
+
+         
+
+  <!-- Template Main JS File -->
+  <script src="../js/main.js">
+
+  </script>
+</body>
+</html>
+
+<?php
+
+// Establish database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "registration";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Validate form submission
+if (isset($_POST['name'], $_POST['email'], $_POST['contact_number'], $_POST['profession']))
+ {
+    // Get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $contactNumber = $_POST['contact_number'];
+    $profession = $_POST['profession'];
+
+    // Prepare and execute SQL query
+    $sql = "INSERT INTO users (name, email, contact_number, profession) VALUES (?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssss", $name, $email, $contactNumber, $profession);
+
+    if ($stmt->execute()) 
+    { ?>      
+    <script>
+      swal({
+        title: "Congratulation!",
+        text: "You has been Registered Successfully!!",
+        icon: "success",
+      });
+      
+      </script>
+       <?php
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Close database connection
+    $stmt->close();
+} else {
+    echo "";
+}
+
+$conn->close();
+?>
+
+
+
